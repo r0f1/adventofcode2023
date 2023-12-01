425019476553d7258786a72b9ab7efd4ba30d1f5
Florian Rohrer <frohrer@exscientia.co.uk>
2023-12-01 12:39:10 +0100

    Day 1

--- day01/main.py
+++ day01/main.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+with open("input.txt") as f:
+    lines = [x.strip() for x in f]
+
+# Part 1
+digits = [[d for d in l if d.isdigit()] for l in lines]
+values = [int(d[0]+d[-1]) for d in digits]
+print(sum(values))
+
+# Part 2
+# difficulty: number strings can be overlapping --> using string replacement
+patterns = [("one", "o1e"), ("two", "t2o"), ("three", "t3e"),
+            ("four", "f4r"), ("five", "f5e"), ("six", "s6x"),
+            ("seven", "s7n"), ("eight", "e8t"), ("nine", "n9e")]
+values = []
+for l in lines:
+    for p in patterns:
+        l = l.replace(*p)
+    ds = [d for d in l if d.isdigit()]
+    values.append(int(ds[0]+ds[-1]))
+print(sum(values))
