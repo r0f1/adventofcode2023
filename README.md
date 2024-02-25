e1651507c1189fbbff17e92e294a910cc42278ab
Florian Rohrer <semicolonator@gmail.com>
2024-02-25 12:36:19 +0100

    Day 3

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 |:---|:---|
 |[Day 1: Trebuchet?!](https://adventofcode.com/2023/day/1)    |[py](/day01/main.py), [alt](/day01/alt.py)|
 |[Day 2: Cube Conundrum](https://adventofcode.com/2023/day/2) |[py](/day02/main.py)|
+|[Day 3: Gear Ratios](https://adventofcode.com/2023/day/3)    |[py](/day03/main.py)|
 
 My solutions from previous years:
 * [r0f1/adventofcode2020](https://github.com/r0f1/adventofcode2020)
--- day03/main.py
+++ day03/main.py
@@ -1,21 +1,38 @@
-import numpy as np
+from pathlib import Path
 import re
-from skimage.morphology import dilation
-from skimage.morphology import square
+import numpy as np
+from skimage.measure import label
+from collections import defaultdict
 
-with open("test.txt") as f:
-    lines = [x.strip() for x in f]
-
-syms = []
+lines = Path("input.txt").read_text().splitlines()
+grid = []
 for l in lines:
-    l = re.sub(r"\d", ".", l)
-    l = l.replace(".", "0")
-    l = re.sub("\W", "1", l)
-    syms.append(l)
+    s = re.sub(r"\d", ".", l)
+    s = s.replace(".", "0")
+    s = re.sub(r"\*", "2", s)
+    s = re.sub(r"\W", "1", s)
+    grid.append(list(s))
+grid = np.array(grid).astype("uint8")
+grid2 = label((grid > 1).astype("uint8"))
 
-valid = np.array([list(s) for s in syms]).astype("uint8")
-valid = dilation(valid, square(3))
-print(valid)
+max_y, max_x = grid.shape
+gears = defaultdict(list)
 
-for l in lines:
-    re.findall("\d+", l)
+res = 0
+for y, line in enumerate(lines):
+    for m in re.finditer(r"\d+", line):
+        x1, x2 = m.start(), m.end()
+        coords = [(x1-1, y), (x2, y)]
+        for i in range(x1-1, x2+1):
+            coords.append((i, y-1))
+            coords.append((i, y+1))
+        coords = [(cx, cy) for cx, cy in coords if cx > 0 and cx < max_x and cy > 0 and cy < max_y]
+        for cx, cy in coords:
+            if grid[cy][cx] > 0:
+                n = int(m.group(0))
+                res += n
+                if grid2[cy][cx] > 0:
+                    gears[grid2[cy][cx]].append(n)
+                break
+print(res)
+print(sum([np.prod(v) for v in gears.values() if len(v) > 1]))
