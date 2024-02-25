6fa14d989694c6e42cb435a5d4ae625155250603
Florian Rohrer <semicolonator@gmail.com>
2024-02-25 16:21:14 +0100

    Day 5 Part 2

--- day05/main.py
+++ day05/main.py
@@ -5,21 +5,45 @@ seeds = list(map(int, lines[0].split(": ")[1].split()))
 maps = []
 for line in lines[2:]:
     if line.endswith(":"):
-        map = {}
+        map = []
         maps.append(map)
     elif len(line) > 0:
-        dest, source, n = [int(i) for i in line.split()]
-        map[source] = (dest, n)
+        map.append([int(i) for i in line.split()])
     
-maps_clean = [dict(sorted(map.items())) for map in maps]
-
 loc = float("inf")
 for seed in seeds:
     k = seed
-    for map in maps_clean:
-        for s, (d, n) in map.items():
+    for map in maps:
+        for d, s, n in map:
             if s <= k < s+n:
                 k += d - s
                 break
     loc = min(k, loc)
+print(loc)
+
+def treat_intervals(map, items):
+    res = []
+    for d, s, n in map:
+      new_items = []
+      while items:
+        a, b = items.pop()
+        before = (a, min(b,s))
+        inter = (max(a, s), min(s+n, b))
+        after = (max(s+n, a), b)
+        
+        if before[1]>before[0]:
+          new_items.append(before)
+        if inter[1]>inter[0]:
+          res.append((inter[0]-s+d, inter[1]-s+d))
+        if after[1]>after[0]:
+          new_items.append(after)
+      items = new_items
+    return res + items
+
+loc = float("inf")
+for seed_start, seed_len in zip(seeds[::2], seeds[1::2]):
+    k = [(seed_start, seed_start+seed_len)]
+    for map in maps:
+        k = treat_intervals(map, k)
+    loc = min(min(k)[0], loc)
 print(loc)
